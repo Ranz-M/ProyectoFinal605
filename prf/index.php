fd7cb92a5c7765fdb54854378088a18a809e3cf9
Ramses <107085728+Ranz-M@users.noreply.github.com>
2023-03-14 15:30:48 -0600

    add repository

---
+++ prf/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+
+    <script src="js/jquery.js"></script
+    <title>Practica1 con php</title>
+</head>
+<body>
+    <script src="js/bootstrap.min.js"></script>
+
+    <?php include "include/navbar.php"?>
+    <?php include "include/menu.php"?>
+</body>
+</html>
